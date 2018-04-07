ebb3801566988cd705288b944b4725f73ac9d205
brainysoon <brainysoon@gmail.com>
2018-04-07 22:59:46 +0800

    [ InverseReduce ] - Generate an array that decrease by 2 until the seed is not great than zero.

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,21 +1,25 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        return IntStream.iterate(number - 2, value -> value - 2)
+                .limit(number / 2)
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
 }
