431d6ee6d1cdfd7af95d192ea22d5227785649bb
brainysoon <brainysoon@gmail.com>
2018-04-07 22:23:55 +0800

    [ Flaten ] - Turn two dimensional array into one dimensional array.

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,17 +2,22 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(this.array)
+                .flatMapToInt(intArray -> Arrays.stream(intArray).mapToInt(Integer::intValue))
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
