895429f5b9ab21a09591f523610a853b7181e911
brainysoon <brainysoon@gmail.com>
2018-04-07 21:02:07 +0800

    [ CollectionOperator ] - Generate array that within the given rang.

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,12 +1,27 @@
 package com.thoughtworks.collection;
 
+import com.thoughtworks.collection.util.BorderUtils;
+import com.thoughtworks.collection.util.BorderUtils.Border;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        Border border = BorderUtils.valueOf(left, right);
+
+        List<Integer> ascendingOrderList = IntStream.rangeClosed(border.getStartValue(), border.getEndValue())
+                .collect(ArrayList::new, List::add, List::addAll);
+        if (left > right) {
+            return ascendingOrderList.stream()
+                    .sorted(Comparator.comparing(Integer::intValue).reversed())
+                    .collect(Collectors.toList());
+        }
+        return ascendingOrderList;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
